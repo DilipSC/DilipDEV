52a3ad9c9f2991b1dbef8a59d3677fecbc82574e
DilipSC <dilipchakravarthi570@gmail.com>
2024-11-23 00:40:52 +0530

    Final port

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['i.ibb.co', 'source.unsplash.com'], // Add i.ibb.co (or ibb.co, depending on the direct image host)
+  },
 };
 
 export default nextConfig;
